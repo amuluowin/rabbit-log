6215a61ddd4780f7b8ba5ceeab28d1953da02ee5
albert <amuluowin@163.com>
2020-02-13 17:17:28 +0800

    add default template

---
+++ src/RegisterTemplateHandler.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace rabbit\log;
+
+use Psr\Http\Message\ServerRequestInterface;
+use rabbit\core\Context;
+use rabbit\helper\ArrayHelper;
+use rabbit\server\AttributeEnum;
+
+/**
+ * Class RegisterTemplateHandler
+ * @package rabbit\log
+ */
+class RegisterTemplateHandler implements TemplateInterface
+{
+    /** @var array */
+    private $possibleStyles = [];
+    /** @var array */
+    private $htmlColors = [];
+
+    /**
+     * RegisterTemplateHandler constructor.
+     */
+    public function __construct()
+    {
+        $this->possibleStyles = (new ConsoleColor())->getPossibleStyles();
+        $this->htmlColors = HtmlColor::getPossibleColors();
+    }
+
+    /**
+     * @return array
+     */
+    public function handle(): array
+    {
+        if (($request = Context::get(Logger::CONTEXT_KEY)) === null) {
+            /** @var ServerRequestInterface $serverRequest */
+            if (($serverRequest = Context::get('request')) !== null) {
+                $uri = $serverRequest->getUri();
+                $requestId = $serverRequest->getAttribute(AttributeEnum::REQUESTID_ATTRIBUTE);
+                !$requestId && $requestId = uniqid();
+                $request = array_filter([
+                    '%Q' => $requestId,
+                    '%R' => $uri->getPath(),
+                    '%m' => $serverRequest->getMethod(),
+                    '%I' => ArrayHelper::getValue($serverRequest->getServerParams(), 'remote_addr'),
+                    '%c' => [
+                        $this->possibleStyles[rand(0, count($this->possibleStyles) - 1)],
+                        $this->htmlColors[rand(0, count($this->htmlColors) - 1)]
+                    ]
+                ]);
+            } else {
+                $request = array_filter([
+                    '%Q' => uniqid(),
+                    '%c' => [
+                        $this->possibleStyles[rand(0, count($this->possibleStyles) - 1)],
+                        $this->htmlColors[rand(0, count($this->htmlColors) - 1)]
+                    ]
+                ]);
+            }
+            Context::set(Logger::CONTEXT_KEY, $request);
+        }
+        return $request;
+    }
+}
