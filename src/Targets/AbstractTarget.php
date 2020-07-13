7dc20886bb385aaf184abe6a663349d180a94fbb
albert <amuluowin@163.com>
2020-07-13 12:18:27 +0800

    add dir

---
+++ src/Targets/AbstractTarget.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+namespace Rabbit\Log\Targets;
+
+use Co\Channel;
+use Rabbit\Base\Contract\InitInterface;
+
+/**
+ * Class AbstractTarget
+ * @package rabbit\log\targets
+ */
+abstract class AbstractTarget implements InitInterface
+{
+    /** @var string */
+    protected string $split = ' | ';
+    /** @var array */
+    protected array $levelList = [];
+    /** @var int */
+    protected int $levelIndex = 1;
+    /** @var Channel */
+    protected Channel $channel;
+    /** @var int */
+    protected int $batch = 100;
+    /** @var float */
+    protected float $waitTime = 1;
+
+    /**
+     * AbstractTarget constructor.
+     * @param string $split
+     */
+    public function __construct(string $split = ' | ')
+    {
+        $this->split = $split;
+        $this->channel = new Channel();
+    }
+
+    public function init()
+    {
+        $this->write();
+    }
+
+    /**
+     * @param Channel|null $channel
+     * @return array
+     */
+    public function getLogs(Channel $channel = null): array
+    {
+        $channel = $channel ?? $this->channel;
+        $logs = [];
+        for ($i = 0; $i < $this->batch; $i++) {
+            $log = $channel->pop($this->waitTime);
+            if ($log === false) {
+                break;
+            }
+            $logs[] = $log;
+        }
+        return $logs;
+    }
+
+
+    /**
+     * @param array $messages
+     */
+    abstract public function export(array $messages): void;
+
+    abstract protected function write(): void;
+}
